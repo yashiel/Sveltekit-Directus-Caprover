90c12970df9f9799fa32fce748d765145d0524c7
Yashi EL <hi@yashiel.com>
2022-05-05 10:44:58 +0530

    index changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,9 @@ export async function get() {
 	const directus = await getDirectusClient();
 	const response = await directus.items('blog').readByQuery({
 				fields: ['*'],
-
+				filter: {
+					status: 'published',
+				}
 
 	});
 	const articles = response.data;
